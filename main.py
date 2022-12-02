bb3236905f27b1a4a30b81c3a32b382f4d8788b0
MrB-Can <paul@bigeye.com>
2022-12-02 08:09:12 -0800

    Completed AWS secrets integration.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 
+from secrets import get_secret
+
+
 # TODO: Change the class to use vraiables to create the data instead of all these methods.
 # TODO: create some sort of class change or method to run all methods and return all data in a tuple of dict.
 # TODO: Create a data engine to get this into a database / flow to see in bigeye
@@ -9,7 +12,7 @@ class SpaceDataTLE:
 
     def __init__(self, norad_id):
         self.api_root = 'https://api.n2yo.com/rest/v1/satellite/'
-        self.api_key = 'R9CBTT-75U9QA-GCFB86-4YNB'
+        self.api_key = get_secret('api_key_n2yo')
         self.norad_id = norad_id
 
     def get_object_tle(self):
